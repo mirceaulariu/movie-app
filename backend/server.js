43201ebfe7dda6b442d851f7ceac36cb98c8015c
mirceaulariu <m.ulariu@gmail.com>
2026-05-26 20:33:15 +0300

    ...

--- backend/server.js
+++ backend/server.js
@@ -15,9 +15,15 @@ app.get('/api/trending', async (req, res) => {
 
     try {
 
-        const url = region
-            ? 'https://api.themoviedb.org/3/movie/popular?api_key=YOUR_KEY&region=${region}&language=en-US'
-            : 'https://api.themoviedb.org/3/trending/movie/day?api_key=YOUR_KEY';
+        if (genre && genre != '') {
+            url = `https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&watch_region=${region}&with_genres=${genre}&language=en-US`;
+        }
+        else {
+            const url = region
+                ? 'https://api.themoviedb.org/3/movie/popular?api_key=YOUR_KEY&region=${region}&language=en-US'
+                : 'https://api.themoviedb.org/3/trending/movie/day?api_key=YOUR_KEY';
+        }
+
 
         const response = await axios.get(
             url,
